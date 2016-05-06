85a91e587a635ec50e1005fd4f4bed266eed386f
likegoto <leikai0391@gmail.com>
2016-05-06 18:36:24 +0800

    资讯界面框架搭建

--- Headmaster/Controller/Information/View/JZInformationListCell.m
+++ Headmaster/Controller/Information/View/JZInformationListCell.m
@@ -18,16 +18,59 @@
 
 @end
 @implementation JZInformationListCell
-
-- (void)awakeFromNib {
-    [super awakeFromNib];
-    // Initialization code
+- (instancetype)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier{
+    if (self = [super initWithStyle:style reuseIdentifier:reuseIdentifier]) {
+        //        self.selectionStyle = UITableViewCellSelectionStyleNone;
+    }
+    return self;
 }
+
 
 - (void)setSelected:(BOOL)selected animated:(BOOL)animated {
     [super setSelected:selected animated:animated];
+// Configure the view for the selected state
+}
 
-    // Configure the view for the selected state
+-(UIImageView *)newsImageView {
+    
+    if (!_newsImageView) {
+        
+        UIImageView *newsImageView = [[UIImageView alloc]init];
+        self.newsImageView = newsImageView;
+        [self.contentView addSubview:newsImageView];
+        
+    }
+    return _newsImageView;
+}
+
+-(UILabel *)newsTitleLabel {
+    if (!_newsTitleLabel) {
+        
+        UILabel *newsTitleLabel = [[UILabel alloc]init];
+        
+        [newsTitleLabel setFont:[UIFont systemFontOfSize:14]];
+        newsTitleLabel.textColor = [UIColor blackColor];
+        self.newsTitleLabel = newsTitleLabel;
+        [self.contentView addSubview:newsTitleLabel];
+        
+    }
+    return _newsTitleLabel;
+
+    
+}
+
+-(UILabel *)newsDateLabel {
+    if (!_newsDateLabel) {
+        
+        UILabel *newsDateLabel = [[UILabel alloc]init];
+        
+        [newsDateLabel setFont:[UIFont systemFontOfSize:14]];
+        newsDateLabel.textColor = [UIColor blackColor];
+        self.newsDateLabel = newsDateLabel;
+        [self.contentView addSubview:newsDateLabel];
+
+    }
+    return _newsDateLabel;
 }
 
 @end
