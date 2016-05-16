63f028d4f941076633e89c74976c1a133b74940d
likegoto <leikai0391@gmail.com>
2016-05-16 15:39:18 +0800

    空白图文件

--- Headmaster/Controller/SideMenu/JZPublishHistoryCell.m
+++ Headmaster/Controller/SideMenu/JZPublishHistoryCell.m
@@ -21,57 +21,62 @@
 
 @property (nonatomic, strong) UILabel *nameLabel;
 
-
-
-
-
 @end
+
 @implementation JZPublishHistoryCell
+
 - (instancetype)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier{
     if (self = [super initWithStyle:style reuseIdentifier:reuseIdentifier]) {
         [self initUI];
-        self.selectionStyle = UITableViewCellSelectionStyleNone;
-        
-//        UIView *bjView = [[UIView alloc]init];
-//        bjView.backgroundColor = [UIColor blackColor];
-//        bjView.alpha = 0.4;
-//        self.backgroundView = bjView;
     }
     return self;
 }
-- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
-    [super setSelected:selected animated:animated];
-    
-    // Configure the view for the selected state
-}
+
 -(void)initUI {
+    
     self.mainTitleLabel = [[UILabel alloc]init];
     self.mainTitleLabel.textAlignment = NSTextAlignmentLeft;
     self.mainTitleLabel.textColor = kJZDarkTextColor;
-    [self.mainTitleLabel setFont:[UIFont systemFontOfSize:14]];
     
     
     self.nameLabel = [[UILabel alloc]init];
     self.nameLabel.textAlignment = NSTextAlignmentRight;
     self.nameLabel.textColor = kJZLightTextColor;
-    [self.nameLabel setFont:[UIFont systemFontOfSize:12]];
     
     self.timeLabel = [[UILabel alloc]init];
     self.timeLabel.textAlignment = NSTextAlignmentLeft;
     self.timeLabel.textColor = kJZLightTextColor;
-    [self.timeLabel setFont:[UIFont systemFontOfSize:12]];
+  
     
     self.contentLabel = [[UILabel alloc]init];
     self.contentLabel.textAlignment = NSTextAlignmentLeft;
     self.contentLabel.textColor = kJZDarkTextColor;
     self.contentLabel.numberOfLines = 0;
-    [self.contentLabel setFont:[UIFont systemFontOfSize:14]];
     
     self.linView = [[UIView alloc]init];
-    self.linView.backgroundColor = kJZLightTextColor;
+    self.linView.backgroundColor = JZ_MAIN_BACKGROUND_COLOR;
     
-//    self.linView.layer.shadowColor = [UIColor darkGrayColor].CGColor;
-//    self.linView.layer.shadowOpacity = 0.5;
+    if (YBIphone6Plus) {
+        
+        [self.mainTitleLabel setFont:[UIFont boldSystemFontOfSize:16*YBRatio]];
+        
+        [self.nameLabel setFont:[UIFont systemFontOfSize:12*YBRatio]];
+        
+        [self.timeLabel setFont:[UIFont systemFontOfSize:12*YBRatio]];
+       
+        [self.contentLabel setFont:[UIFont systemFontOfSize:14*YBRatio]];
+
+    }else {
+        
+        [self.mainTitleLabel setFont:[UIFont boldSystemFontOfSize:16]];
+        
+        [self.nameLabel setFont:[UIFont systemFontOfSize:12]];
+        
+        [self.timeLabel setFont:[UIFont systemFontOfSize:12]];
+      
+        [self.contentLabel setFont:[UIFont systemFontOfSize:14]];
+
+    }
     
     
     [self.contentView addSubview:self.mainTitleLabel];
@@ -80,91 +85,69 @@
     [self.contentView addSubview:self.linView];
     [self.contentView addSubview:self.nameLabel];
     
-    
-}
-
--(void)layoutSubviews {
-    
-    [super layoutSubviews];
-    
-    [self.mainTitleLabel mas_makeConstraints:^(MASConstraintMaker *make) {
-       
-        make.top.equalTo(self.contentView.mas_top).offset(14);//
-        make.left.equalTo(self.contentView.mas_left).offset(16);
-        
-    }];
-    
-    [self.timeLabel mas_makeConstraints:^(MASConstraintMaker *make) {
-       
-        make.top.equalTo(self.mainTitleLabel.mas_bottom).offset(10);//
-        make.left.equalTo(self.contentView.mas_left).offset(16);
-        
-    }];
-    
-    [self.nameLabel mas_makeConstraints:^(MASConstraintMaker *make) {
-        
-        make.top.equalTo(self.mainTitleLabel.mas_bottom).offset(10);
-        make.right.equalTo(self.contentView.mas_right).offset(-16);
-
-    }];
-    
-    [self.contentLabel mas_makeConstraints:^(MASConstraintMaker *make) {
-        
-        make.top.equalTo(self.timeLabel.mas_bottom).offset(12);//
-        make.left.equalTo(self.contentView.mas_left).offset(16);
-        make.right.equalTo(self.contentView.mas_right).offset(-16);
-
-    }];
-    
-    [self.linView mas_makeConstraints:^(MASConstraintMaker *make) {
-        
-        make.left.equalTo(self.contentView.mas_left).offset(0);
-        make.right.equalTo(self.contentView.mas_right).offset(0);
-        make.top.equalTo(self.contentLabel.mas_bottom).offset(14);
-        make.height.equalTo(@0.5);
-        
-    }];
-
-    
 }
 
 -(void)setData:(JZPublishHistoryData *)data {
     
     _data = data;
     
+    NSString *str1 = @"无标题";
+
+    if (_data.title && [_data.title length]!=0) {
+        str1 = _data.title;
+    }
     
+    self.mainTitleLabel.text = str1;
     
-    if ([_data.title isEqualToString:@""]) {
-        
-        NSString *str1 = @"无标题";
-        UIFont *boldFont = [UIFont boldSystemFontOfSize:16];
-        NSMutableAttributedString *attrString = [[NSMutableAttributedString alloc] initWithString:@"无标题"];
-        [attrString addAttribute:NSFontAttributeName value:boldFont range:NSMakeRange(0, str1.length)];
-        [self.mainTitleLabel setAttributedText:attrString];
-        
+    NSLog(@"_data.content:(%@)",_data.content);
+          
+    self.timeLabel.text = [self getYearLocalDateFormateUTCDate:_data.createtime];
+
+    self.contentLabel.text = _data.content;
+    
+    if (_data.name && _data.name.length) {
+      self.nameLabel.text = [NSString stringWithFormat:@"发布者: %@",_data.name];
     }else {
-        NSString *str2 = _data.title;
-        UIFont *boldFont = [UIFont boldSystemFontOfSize:16];
-        NSMutableAttributedString *attrString = [[NSMutableAttributedString alloc] initWithString:str2];
-        [attrString addAttribute:NSFontAttributeName value:boldFont range:NSMakeRange(0, str2.length)];
-        [self.mainTitleLabel setAttributedText:attrString];
+        self.nameLabel.text = @"发布者: 未知";
     }
     
     
-    self.contentLabel.text = _data.content;
-    self.timeLabel.text = [self getYearLocalDateFormateUTCDate:_data.createtime];
-
+    [self.mainTitleLabel mas_remakeConstraints:^(MASConstraintMaker *make) {
+        
+        make.top.equalTo(self.contentView.mas_top).offset(24);// 24
+        make.left.equalTo(self.contentView.mas_left).offset(16);
+        
+    }];
+    
+    [self.timeLabel mas_remakeConstraints:^(MASConstraintMaker *make) {
+        
+        make.top.equalTo(self.mainTitleLabel.mas_bottom).offset(12);// 12
+        make.left.equalTo(self.contentView.mas_left).offset(16);
+        
+    }];
+    
+    [self.nameLabel mas_remakeConstraints:^(MASConstraintMaker *make) {
+        
+        make.top.equalTo(self.mainTitleLabel.mas_bottom).offset(12);
+        make.right.equalTo(self.contentView.mas_right).offset(-16);
+        
+    }];
+    
     [self.contentLabel mas_remakeConstraints:^(MASConstraintMaker *make) {
         
-        make.top.equalTo(self.timeLabel.mas_bottom).offset(12);
+        make.top.equalTo(self.timeLabel.mas_bottom).offset(14);// 14
         make.left.equalTo(self.contentView.mas_left).offset(16);
         make.right.equalTo(self.contentView.mas_right).offset(-16);
         
     }];
     
-   
-    self.nameLabel.text = [NSString stringWithFormat:@"发布者: %@",_data.name];
-    
+    [self.linView mas_remakeConstraints:^(MASConstraintMaker *make) {
+        make.left.equalTo(self.contentView.mas_left).offset(0);
+        make.right.equalTo(self.contentView.mas_right).offset(0);
+        make.top.equalTo(self.contentLabel.mas_bottom).offset(24);// 24
+        make.height.equalTo(@0.5);
+    }];
+
 }
 
 + (CGFloat)cellHeightDmData:(JZPublishHistoryData *)dmData
@@ -176,7 +159,10 @@
     
     [cell layoutIfNeeded];
 
-    return cell.timeLabel.height + cell.mainTitleLabel.height + cell.contentLabel.height  + 0.5 + 50;
+//    CGFloat height = [dmData.content sizeWithFont:[UIFont boldSystemFontOfSize:14*YBRatio] constrainedToSize:CGSizeMake(cell.contentView.width - 16 - 16, MAXFLOAT)].height;
+//    NSLog(@"---height:%f",height);
+    
+    return cell.timeLabel.height + cell.mainTitleLabel.height + cell.contentLabel.height + 0.5 + 74;
     
 }
 
