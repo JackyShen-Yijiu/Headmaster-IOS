1e2a094ec0a2da91931d27b08eb75a0baf1030a7
likegoto <leikai0391@gmail.com>
2016-05-19 13:35:39 +0800

    投诉详情--查看大图代码封装

--- Headmaster/Controller/ComplaintDetail/View/JZComplaintDetailView.m
+++ Headmaster/Controller/ComplaintDetail/View/JZComplaintDetailView.m
@@ -10,6 +10,7 @@
 #import "JZComplaintComplaintlist.h"
 //#import "MWPhotoBrowser.h"
 #import "NSString+LKString.h"
+#import "UIView+LKView.h"
 
 @interface JZComplaintDetailView ()
 ///"投诉内容"这四个文字
@@ -106,49 +107,17 @@
     JZComplaintComplaintlist *data = self.data;
     NSString *picStr = data.piclistr[0];
     
-    [self showBigImageStr:picStr];
+    [UIView showBigImageStr:picStr];
     
 }
 -(void)seeSecondBigImg {
     JZComplaintComplaintlist *data = self.data;
     NSString *picStr = data.piclistr[1];
     
-    [self showBigImageStr:picStr];
+    [UIView showBigImageStr:picStr];
+
 }
 
-- (void)showBigImageStr:(NSString *)imagestr {
-    
-    UIView *bgView = [[UIView alloc] initWithFrame:[UIScreen mainScreen].bounds];
-    UITapGestureRecognizer *tapGes = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(closeBigImg:)];
-    [bgView addGestureRecognizer:tapGes];
-    bgView.userInteractionEnabled = YES;
-    bgView.backgroundColor = [UIColor blackColor];
-    bgView.alpha = 0;
-    UIImageView *imageView = [UIImageView new];
-    imageView.bounds = CGRectMake(0, 0, 0, 0);
-    imageView.center = bgView.center;
-    [imageView sd_setImageWithURL:[NSURL URLWithString:imagestr] placeholderImage:[UIImage imageNamed:@"pic_load"]];
-    [bgView addSubview:imageView];
-    [[UIApplication sharedApplication].keyWindow addSubview:bgView];
-    
-    [UIView animateWithDuration:0.3 delay:0 options:UIViewAnimationOptionCurveEaseInOut animations:^{
-        
-        bgView.alpha = 1;
-        imageView.bounds = CGRectMake(0, 0, kJZWidth, kJZHeight*0.7);
-        
-    } completion:^(BOOL finished) {
-        
-    }];
-}
-- (void)closeBigImg:(UITapGestureRecognizer *)tagGes {
-    
-    UIView *view = tagGes.view;
-    [UIView animateWithDuration:0.3 animations:^{
-        view.alpha = 0;
-    } completion:^(BOOL finished) {
-        [tagGes.view removeFromSuperview];
-    }];
-}
 
 
 #pragma mark - 懒加载
--- Headmaster/Controller/JZMailbox/LKLibBox/LKViewCategory/UIView+LKView.h
+++ Headmaster/Controller/JZMailbox/LKLibBox/LKViewCategory/UIView+LKView.h
@@ -9,5 +9,6 @@
 #import <UIKit/UIKit.h>
 
 @interface UIView (LKView)
-
++ (void)showBigImageStr:(NSString *)imagestr;
++ (void)closeBigImg:(UITapGestureRecognizer *)tagGes;
 @end
